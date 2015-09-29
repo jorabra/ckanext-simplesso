9f2d79ebb77c747f08ae0e1fe589da966a108d20
Jørgen Abrahamsen <jorgen.abrahamsen@gmail.com>
2015-09-29 12:58:13 +0200

    Add and remove newline and whitespace.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 import sys, os
 
+
 version = '0.1'
 
 setup(
     name='ckanext-simplesso',
     version=version,
-    description="Use a header to login to CKAN",
+    description='Use a header to login to CKAN',
     long_description='''
     ''',
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
